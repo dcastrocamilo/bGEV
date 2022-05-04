977ea1bafe24160a8245440d9bb0cbeb8b67a192
Daniela Castro Camilo <danielacastrocamilo@Danielas-MacBook-Air.local>
2022-05-04 09:26:12 +0100

    Changes to the computation of return levels

--- Simulation/Sim1.R
+++ Simulation/Sim1.R
@@ -17,7 +17,6 @@ p_a   = 0.05
 p_b   = 0.2
 s     = 5
 rp    = c(30,50,100)
-rl    = evd::qgev(1/rp, mu, sigma, xi, lower.tail = F) # true rls
 
 ## Simulation scenarios
 n = c(30,50,100,500)
@@ -34,9 +33,9 @@ for(mc in 1:M){
   
   for(i in 1:nrow(scenarios)){
     print(paste('Scenario', i))
-    N = scenarios$N[i]
-    n = scenarios$n[i]
-    y = rep(NA, N)
+    N  = scenarios$N[i]
+    n  = scenarios$n[i]
+    y  = rep(NA, N)
     for(j in 1:N){
       set.seed(n*j+mc)
       y[j] = max(evd::rfrechet(n, shape = alpha))
@@ -90,36 +89,38 @@ rl.bgev30  = read.table('Simulation/Sim1/sim1out_bgev_rl30.txt', header = F); di
 rl.bgev50  = read.table('Simulation/Sim1/sim1out_bgev_rl50.txt', header = F); dim(rl.bgev50)
 rl.bgev100 = read.table('Simulation/Sim1/sim1out_bgev_rl100.txt', header = F); dim(rl.bgev100)
 
-rmse.bgev30  = round(sqrt(colMeans((rl.bgev30 - rl[1])^2)),2)
-rmse.bgev50  = round(sqrt(colMeans((rl.bgev50 - rl[2])^2)),2)
-rmse.bgev100 = round(sqrt(colMeans((rl.bgev100 - rl[3])^2)),2)
-
-rmse.bgev30           = data.frame(matrix(rmse.bgev30, ncol = length(N)))
-rmse.bgev50           = data.frame(matrix(rmse.bgev50, ncol = length(N)))
-rmse.bgev100           = data.frame(matrix(rmse.bgev100, ncol = length(N)))
-colnames(rmse.bgev30) = colnames(rmse.bgev50) = colnames(rmse.bgev100) = N
-rownames(rmse.bgev30) = rownames(rmse.bgev50) = rownames(rmse.bgev100) = n
-rmse.bgev30
-rmse.bgev50
-rmse.bgev100
-
 ## RMSE gev
 rl.gev30  = read.table('Simulation/Sim1/sim1out_gev_rl30.txt', header = F); dim(rl.gev30)
 rl.gev50  = read.table('Simulation/Sim1/sim1out_gev_rl50.txt', header = F); dim(rl.gev50)
 rl.gev100 = read.table('Simulation/Sim1/sim1out_gev_rl100.txt', header = F); dim(rl.gev100)
 
-rmse.gev30  = round(sqrt(colMeans((rl.gev30 - rl[1])^2)),2)
-rmse.gev50  = round(sqrt(colMeans((rl.gev50 - rl[2])^2)),2)
-rmse.gev100 = round(sqrt(colMeans((rl.gev100 - rl[3])^2)),2)
+
+rmse.bgev30 = rmse.bgev50 = rmse.bgev100 = rmse.gev30 = rmse.gev50 = rmse.gev100 = rep(NA, nrow(scenarios))
+for(i in 1:nrow(scenarios)){
+  nn  = scenarios$n[i]
+  rl = evd::qfrechet(1/rp, mu, nn^(1/alpha)*sigma, alpha) # rls at the limit
+  
+  rmse.bgev30[i]  = round(sqrt(mean((rl.bgev30[,i] - rl[1])^2)),2)
+  rmse.bgev50[i]  = round(sqrt(mean((rl.bgev50[,i] - rl[2])^2)),2)
+  rmse.bgev100[i] = round(sqrt(mean((rl.bgev100[,i] - rl[3])^2)),2)
+  
+  rmse.gev30[i]   = round(sqrt(mean((rl.gev30[,i] - rl[1])^2)),2)
+  rmse.gev50[i]   = round(sqrt(mean((rl.gev50[,i] - rl[2])^2)),2)
+  rmse.gev100[i]  = round(sqrt(mean((rl.gev100[,i] - rl[3])^2)),2)
+  
+}
+cbind(scenarios, rmse.bgev30)
+rmse.bgev30           = data.frame(matrix(rmse.bgev30, ncol = length(N)))
+rmse.bgev50           = data.frame(matrix(rmse.bgev50, ncol = length(N)))
+rmse.bgev100          = data.frame(matrix(rmse.bgev100, ncol = length(N)))
+colnames(rmse.bgev30) = colnames(rmse.bgev50) = colnames(rmse.bgev100) = N
+rownames(rmse.bgev30) = rownames(rmse.bgev50) = rownames(rmse.bgev100) = n
 
 rmse.gev30           = data.frame(matrix(rmse.gev30, ncol = length(N)))
 rmse.gev50           = data.frame(matrix(rmse.gev50, ncol = length(N)))
 rmse.gev100          = data.frame(matrix(rmse.gev100, ncol = length(N)))
 colnames(rmse.gev30) = colnames(rmse.gev50) = colnames(rmse.gev100) = N
 rownames(rmse.gev30) = rownames(rmse.gev50) = rownames(rmse.gev100) = n
-rmse.gev30
-rmse.gev50
-rmse.gev100
 
 ## Difference
 rmse30  = rmse.gev30 -rmse.bgev30; rmse30
@@ -133,13 +134,5 @@ rmse$`100`  = paste0(round(rmse30[,3],2), '/', round(rmse50[,3],2), '/', round(r
 rmse$`500`  = paste0(round(rmse30[,4],2), '/', round(rmse50[,4],2), '/', round(rmse100[,4],2))
 rmse$`1000` = paste0(round(rmse30[,5],2), '/', round(rmse50[,5],2), '/', round(rmse100[,5],2))
 xtable(rmse)
-
-
-
-
-
-
-
-
 
 
